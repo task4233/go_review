1b860a26ea69f028357981ea84de677950b3bd47
task4233 <tks.m1205@gmail.com>
2019-02-08 13:29:28 +0000

    [UPDATE] main.go /message

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
+// こんな感じでまとめてインポートすることも可能
 import (
   "fmt"
   "../go_review/message"
 )
 
+// main関数があるものが最初によばれるよ
+// `go run`で呼ぶファイルはこのmain関数がないと許されないよ
+// そんなことをしたらセグ木の下に埋めてもらって構わないよ!
 func main() {
   fmt.Println(message.GetHelloMessage("hoge"))
 }
